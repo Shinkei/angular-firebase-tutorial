3bc8c12936508c57ba5f8598c3c0b312bd482f80
Shinkei <ing.jorgeivanramirezh@gmail.com>
2017-09-07 23:02:01 -0500

    based configuration for angular firebase tutorial

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,27 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { RouterModule, Routes } from '@angular/router';
 
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
+
+const appRoutes: Routes = [
+  // {path: '', component: HomeComponent},
+  // {path: 'characters', component: CharactersComponent},
+  // {path: 'add-character', component: AddCharacterComponent},
+  // {path: 'character/:id', component: CharacterComponent}
+];
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule,
+    RouterModule.forRoot(appRoutes)
   ],
   providers: [],
   bootstrap: [AppComponent]
